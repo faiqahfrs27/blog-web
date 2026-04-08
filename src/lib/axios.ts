d8244bb183a057d5dce57439119cbe4bc12df4db
faiqahfrs27 <faiqah.farrasf@gmail.com>
2026-04-08 21:35:46 +0700

    initial commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://rightguitar-us.backendless.app/api",
 });
+export const axiosInstance2 = axios.create({
+  baseURL: "http://localhost:8000",
+});
 
-// bikir84466@iaciu.com
-// Admin123
+//dudu@mail.com
+//Admin123
